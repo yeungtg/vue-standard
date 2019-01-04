8c5d82f55e12013d80412e30d9828af6e9cd92f5
yeungtg <527734392@qq.com>
2019-01-04 19:03:12 +0800

    refactor(build): 增加commit错误提示

--- build.sh
+++ build.sh
@@ -1,11 +1,16 @@
-#!/usr/bin/env sh
+#!/bin/bash
 
 # 确保脚本抛出遇到的错误
 set -e
+if [ "$1" ]; then
+    echo -e "\033[0;32m commit '$1' \033[0m"
+else
+  echo -e "\033[0;31m 请输入commit内容 \n\033[5;31m 例如：sh build.sh '修改config, bugfix#1' \033[0m"
+  exit;
+fi
 
 # 生成静态文件
 npm run build
-
 # 进入生成的文件夹
 cd docs/.vuepress/dist
 
@@ -14,8 +19,8 @@ cd docs/.vuepress/dist
 
 git init
 git add -A
-git commit -m '增加评论功能'
+git commit -m "$1"
 
 
-# 如果发布到 https://<USERNAME>.github.io/<REPO>
+# # 如果发布到 https://<USERNAME>.github.io/<REPO>
 git push -f git@github.com:yeungtg/vue-standard.git master:gh-pages
